7e28e10d542d9e89666e8d39a20073b3106f7f3a
Hericles <hericlesreal@gmail.com>
2026-04-14 21:30:27 -0300

    Solução mais ou menos

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -9,7 +9,7 @@ public class Reservation {
     private Date checkIn;
     private Date checkOut;
 
-    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
     public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
         this.roomNumber = roomNumber;
@@ -37,12 +37,22 @@ public class Reservation {
         long diff = checkOut.getTime() - checkIn.getTime();
 
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
-
     }
 
-    public void updateDate(Date checkIn, Date checkOut) {
+    public String updateDate(Date checkIn, Date checkOut) {
+        Date now = new Date();
+        if(checkIn.before(now) || checkOut.before(now)) {
+            return "Error in reservation: Reservation dates for update must be future dates";
+
+        }
+        if(!checkOut.after(checkIn)) {
+            return "Error in reservation: Check-out date must be after check-in date";
+        }
+
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+
+        return null;
 
     }
 
